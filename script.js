34700c62268f5b0e2f478a3019689246a95f281f
Hungry_munk <abbaszadeharsam@gmail.com>
2022-05-23 12:10:48 +1000

    finished everything I could without chagning things that wopuld take forever

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+const root = document.querySelector(':root')
 const moon = document.getElementById('moon')
 //crating sun icon
 const sun = document.createElement('i')
-sun.classList.add('fa-regular','fa-sun','fa-2x')
+sun.classList.add('fa-solid','fa-sun','fa-2x')
 
 
 moon.addEventListener('click' , ()=> {
     moon.replaceWith(sun)
+    root.style.setProperty('--font-colour','white');
+    root.style.setProperty('--background-gray','black');
+    root.style.setProperty('--background-white','#363636')
 })
 
 sun.addEventListener('click' , ()=> {
     sun.replaceWith(moon)
+    root.style.setProperty('--font-colour','black');
+    root.style.setProperty('--background-gray','#E2E8F0');
+    root.style.setProperty('--background-white','#fff')
 })
